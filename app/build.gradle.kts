479b1bc9aa922db8ca7dc967edd0ad0e9ce312b1
SomeshSampat2 <someshsampat1234@gmail.com>
2024-11-18 19:06:24 +0530

    Release 1.3 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.innovatelabs.chesstwist"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 13
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
